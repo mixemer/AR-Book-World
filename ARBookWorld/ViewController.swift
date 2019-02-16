85de33a60c4b4548bb8279ac2fbdecdde200d81c
mixemer <mehmetshin96@gmail.com>
2019-02-16 14:44:19 -0500

    Create the project, and added the basic AR functionality

--- ARBookWorld/ViewController.swift
+++ ARBookWorld/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    
+    private var currentAngleY: Float = 0.0
+    
+    var trackedNode: SCNNode?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,19 +28,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        sceneView.autoenablesDefaultLighting = true
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        let rotate = UIRotationGestureRecognizer(target: self, action:     #selector(rotateNode(_:)))
+        
+        sceneView.addGestureRecognizer(rotate)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "History Cards", bundle: Bundle.main) {
+            
+            configuration.trackingImages = imageToTrack
+            
+            configuration.maximumNumberOfTrackedImages = 2
+            
+            print("Images Successfully Added")
+            
+            
+        }
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -46,30 +62,61 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
+    
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
         let node = SCNNode()
-     
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            node.addChildNode(planeNode)
+            
+            if imageAnchor.referenceImage.name == "skull-card" {
+                if let pokeScene = SCNScene(named: "art.scnassets/triceratops.scn") {
+                    
+                    if let pokeNode = pokeScene.rootNode.childNodes.first {
+                        
+                        //pokeNode.eulerAngles.x = .pi / 2
+                        
+                        planeNode.addChildNode(pokeNode)
+                        
+                        trackedNode = planeNode
+                        
+                    }
+                }
+            }
+        }
         return node
     }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+
+    @objc func rotateNode(_ gesture: UIRotationGestureRecognizer){
         
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        //1. Get The Current Rotation From The Gesture
+        let rotation = Float(gesture.rotation)
         
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        guard let tracked = trackedNode else { return }
         
+        //2. If The Gesture State Has Changed Set The Nodes EulerAngles.y
+        if gesture.state == .changed{
+            
+            tracked.eulerAngles.y = currentAngleY + rotation
+        }
+        
+        //3. If The Gesture Has Ended Store The Last Angle Of The Cube
+        if(gesture.state == .ended) {
+            
+            currentAngleY = tracked.eulerAngles.y
+            
+        }
     }
 }
