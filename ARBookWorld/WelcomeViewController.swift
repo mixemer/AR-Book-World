7a65aa2b292e3e208836607a01487a1e3b61038c
mixemer <mehmetshin96@gmail.com>
2019-02-16 16:39:48 -0500

    Auth for sign up using email

--- ARBookWorld/WelcomeViewController.swift
+++ ARBookWorld/WelcomeViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,5 +30,37 @@ class WelcomeViewController: UIViewController {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
     }
-
+    
+    
+    @IBAction func signInButtonTapped(_ sender: UIButton) {
+        
+        // if firebase, then perform segue
+        
+        performSegue(withIdentifier: "toMyBooks", sender: self)
+    }
+    
+    
+    @IBAction func signUpButtonTapped(_ sender: UIButton) {
+        
+        SVProgressHUD.show()
+        
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) {
+            (result, error) in
+            
+            if error != nil {
+                print("Error: \(error!)")
+            }
+            else {
+                print("Registration succesful")
+                
+                SVProgressHUD.dismiss()
+                
+                self.emailTextField.text = ""
+                self.passwordTextField.text = ""
+                
+                self.performSegue(withIdentifier: "toMyBooks", sender: self)
+            }
+        }
+    }
+    
 }
