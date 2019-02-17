e0ff109318a6522ea4301434223dee50c98a1080
mixemer <mehmetshin96@gmail.com>
2019-02-16 19:54:17 -0500

    Add Firebase as the a data base for the Feed tab bar

--- ARBookWorld/Control/FeedTableViewController.swift
+++ ARBookWorld/Control/FeedTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedTableViewController: UITableViewController {
     
@@ -17,7 +18,7 @@ class FeedTableViewController: UITableViewController {
         
         tableView.register(UINib(nibName: "CustomFeed", bundle: nil), forCellReuseIdentifier: "customFeedCell")
         configureTableView()
-        ///retrieveMessages()
+        retrieveMessages()
         
         tableView.separatorStyle = .none
     }
@@ -29,7 +30,7 @@ class FeedTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return feedArray.count
     }
     
     //TODO: Declare configureTableView here:
@@ -42,15 +43,81 @@ class FeedTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customFeedCell", for: indexPath) as! CustomFeed
         
-        cell.messageBody.text = "Something"
-        cell.senderUsername.text = "Mehmet"
+        cell.messageBody.text = feedArray[indexPath.row].message
+        cell.senderUsername.text = feedArray[indexPath.row].userName
         cell.avatarImageView.image = UIImage(named: "egg")
-
-        // Configure the cell...
 
         return cell
     }
- 
+    
+    @IBAction func logOutBarButtonTapped(_ sender: UIBarButtonItem) {
+        
+        do {
+            try Auth.auth().signOut()
+            tabBarController?.dismiss(animated: true, completion: nil)
+        }
+        catch {
+            print("Error: there was a problem  signing out")
+        }
+        
+    }
+    
+    
+    @IBAction func addBarButtonTapped(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add a new feed", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+//            let newFeed = FeedMessage()
+//            newFeed.message = textField.text!
+//            newFeed.userName = "Test"
+//
+//            self.feedArray.append(newFeed)
+//            self.tableView.reloadData()
+            
+            let feedDB = Database.database().reference().child("Feeds")
+            let feedDictonary = ["Sender":Auth.auth().currentUser?.email, "Feed":textField.text!]
+            
+            feedDB.childByAutoId().setValue(feedDictonary) {
+                (error, database) in
+                
+                if error != nil {
+                    print("Error: \(error!)")
+                }
+                else {
+                    print("Feed saved succesfully")
+                }
+            }
+        }
+        
+        alert.addTextField { (field) in
+            textField = field
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+    func retrieveMessages() {
+        let feedDB = Database.database().reference().child("Feeds")
+        
+        feedDB.observe(.childAdded) {
+            (snapshot) in
+            let value = snapshot.value as! Dictionary<String, String>
+            let feedValue = value["Feed"]!
+            let senderValue = value["Sender"]!
+            
+            let feed = FeedMessage()
+            feed.message = feedValue
+            feed.userName = senderValue
+            
+            self.feedArray.append(feed)
+            self.configureTableView()
+            self.tableView.reloadData()
+        }
+    }
 
     /*
     // Override to support conditional editing of the table view.
